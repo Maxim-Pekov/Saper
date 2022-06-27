e37de069e70461da4bda624e955a7db21a11d079
Maxim-Pekov <max.pekov@gmail.com>
2022-06-28 00:36:26 +0300

    создание классов кнопки и окна

--- main.py
+++ main.py
@@ -1,19 +1,46 @@
 import tkinter as tk
 
-win = tk.Tk()
-win.title('Сапёр')
+class MyButton(tk.Button):
+    def __init__(self, master, x, y, *args, **kwargs):
+        super(MyButton, self).__init__(master, *args, **kwargs)
+        self.x = x
+        self.y = y
+        self.is_mine = False
+    def __repr__(self):
+        return f'btn {self.x} {self.y}'
 
-ROW = 5
-COLUMN = 5
+class MinesKeeper:
 
-buttons = []
-for i in range(ROW):
-    temp=[]
-    for j in range(COLUMN):
-        btn = tk.Button(win, font=('Arial', 15, 'bold'), width=3)
-        btn.grid(row=i, column=j)
-        temp.append(btn)
-    buttons.append(temp)
+    win = tk.Tk()
+    ROW = 5
+    COLUMN = 5
+
+    def __init__(self):
+        self.win.title('Сапёр')
+        self.buttons = []
+        for i in range(MinesKeeper.ROW):
+            temp = []
+            for j in range(MinesKeeper.COLUMN):
+                btn = MyButton(MinesKeeper.win, i, j, font=('Arial', 15, 'bold'), width=3)
+                btn.grid(row=i, column=j)
+                temp.append(btn)
+            self.buttons.append(temp)
+
+    # def create_button(self):
+    #     for i in range(MinesKeeper.ROW):
+    #         for j in range(MinesKeeper.COLUMN):
+    #             btn = self.buttons[i][j]
+    #             btn.grid(row=i, column=j)
+
+    def print_btn(self):
+        for i in self.buttons:
+            print(i)
+
+    def start(self):
+        # self.create_button()
+        self.print_btn()
+        MinesKeeper.win.mainloop()
 
 
-win.mainloop()
+game = MinesKeeper()
+game.start()
