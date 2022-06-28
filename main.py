7044051b8850770a412e551fe094b6817379ae19
Maxim-Pekov <max.pekov@gmail.com>
2022-06-28 11:11:23 +0300

    Отображение координат кнопок в консоли

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 
 class MyButton(tk.Button):
-    def __init__(self, master, x, y, *args, **kwargs):
+    def __init__(self, master, x, y, btn_number, *args, **kwargs):
         super(MyButton, self).__init__(master, *args, **kwargs)
         self.x = x
         self.y = y
         self.is_mine = False
+        self.btn_number = btn_number
     def __repr__(self):
-        return f'btn {self.x} {self.y}'
+        return f'btn {self.x} {self.y} {self.btn_number} {self.is_mine}'
 
 class MinesKeeper:
 
@@ -18,12 +19,15 @@ class MinesKeeper:
     def __init__(self):
         self.win.title('Сапёр')
         self.buttons = []
+        self.count = 1
+        self.mines
         for i in range(MinesKeeper.ROW):
             temp = []
             for j in range(MinesKeeper.COLUMN):
-                btn = MyButton(MinesKeeper.win, i, j, font=('Arial', 15, 'bold'), width=3)
+                btn = MyButton(MinesKeeper.win, i, j, self.count, font=('Arial', 15, 'bold'), width=3)
                 btn.grid(row=i, column=j)
                 temp.append(btn)
+                self.count += 1
             self.buttons.append(temp)
 
     # def create_button(self):
